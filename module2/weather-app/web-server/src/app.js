6f0aac1d1b976303f3b9b3e8002f875738b3360a
sypai <7suyashbajpai@gmail.com>
2023-03-25 16:49:21 +0530

    [add] Handlebar templates for our weather-app routes
    - HBS Partials : header and footer
    - Separate Views and Partials in a templates folder

--- module2/weather-app/web-server/src/app.js
+++ module2/weather-app/web-server/src/app.js
@@ -1,21 +1,47 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs');
 
 // Path to Static files
-public_dir_path = path.join(__dirname, '../public')
+const public_dir_path = path.join(__dirname, '../public')
+const views_path = path.join(__dirname, '../templates/views')
+const partial_path = path.join(__dirname, '../templates/partials')
 
 // Creating our express application
 const app = express()
 
 // Setting Template Engine - hbs
 app.set('view engine', 'hbs')
+app.set('views', views_path)
+hbs.registerPartials(partial_path)
 
 // Setting directory path to be used for static files
 app.use(express.static(public_dir_path))
 
 // Route '/'
 app.get('', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        title: 'Weather',
+        name: 'Suyash'
+    })
+})
+
+// Route '/about'
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About us',
+        name: 'Suyash'
+    })
+})
+
+// Route '/help'
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'Suyash'
+    })
 })
 
 app.get('/weather', (req, res) => {
